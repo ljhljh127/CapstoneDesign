be787c5b3bafdeac183bcad700ea4400411eec70
ljhljh127 <ljhljh1127@naver.com>
2023-08-20 13:29:33 +0000

    cctv 주소 변경 추가

--- PythonRestAPI/README.md
+++ PythonRestAPI/README.md
@@ -5,5 +5,5 @@
 Python을 이용하여 쿠버네티스 API에 CRUD 요청을 보내기 위한 핵심코드
 
 ---
-### Create_CCTV.py
+### Create&Update_CCTV.py
 CCTV를 추가하는 코드(ConfigMap Update, Create Deployment)
--- PythonRestAPI/k8sAPI.py
+++ PythonRestAPI/k8sAPI.py
@@ -1,7 +1,14 @@
 import requests
 import os
 
-def call_k8s_api_post(URL, JWT_TOKEN, resource_type, resource_data):
+# 전역적으로 사용할 URL,JWT_TOKEN 값 정의 
+with open("../../token/Kub.txt", "r") as file:
+    lines = file.readlines()
+    URL = lines[0].strip().split("=")[1]
+    JWT_TOKEN = lines[1].strip().split("=")[1]
+
+
+def call_k8s_api_post(resource_type, resource_data):
     api = f"{URL}/apis/apps/v1/namespaces/cctv/{resource_type}"
     headers = {"Authorization": f"Bearer {JWT_TOKEN}", "Content-Type": "application/json"}
     try:
@@ -15,10 +22,9 @@ def call_k8s_api_post(URL, JWT_TOKEN, resource_type, resource_data):
     return response
 
 
-def call_k8s_api_put(URL, JWT_TOKEN, resource_type, resource_name, resource_data):
+def call_k8s_api_put(resource_type, resource_name, resource_data):
     api = f"{URL}/api/v1/namespaces/cctv/{resource_type}/{resource_name}"
     headers = {"Authorization": f"Bearer {JWT_TOKEN}",  "Content-Type": "application/merge-patch+json"}
-
     try:
         response = requests.patch(api, json=resource_data, headers=headers, verify=False)
     except Exception as e:
@@ -28,7 +34,23 @@ def call_k8s_api_put(URL, JWT_TOKEN, resource_type, resource_name, resource_data
 
     return response
 
-def call_k8s_api_get(URL, JWT_TOKEN, resource_type):
+
+# deployment update apps/v1 대상 api
+def call_k8s_api_put_apps_v1(resource_type, resource_name, resource_data):
+    api = f"{URL}/apis/apps/v1/namespaces/cctv/{resource_type}/{resource_name}"
+    headers = {"Authorization": f"Bearer {JWT_TOKEN}",  "Content-Type": "application/merge-patch+json"}
+    try:
+        response = requests.patch(api, json=resource_data, headers=headers, verify=False)
+
+    except Exception as e:
+       raise Exception(f"Update {resource_type} failed: {e}")
+    if not response.ok:
+        raise Exception("Status is not ok")
+
+    return response
+
+
+def call_k8s_api_get(resource_type):
     api = f"{URL}/api/v1/namespaces/cctv/{resource_type}"
     headers = {"Authorization": f"Bearer {JWT_TOKEN}", "Accept": "application/json"}
 
